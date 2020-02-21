6b33fb983192caa0e8d6c91f7aef4a6a0a5f3e19
NoSeK2323 <45162082+NoSeK2323@users.noreply.github.com>
2020-02-21 12:24:05 +0100

    SH file

---
+++ install.sh
@@ -0,0 +1,7 @@
+# Script made by NoSeK2323 
+sudo apt install edubuntu-desktop
+sudo apt install scratch 
+sudo apt install libreoffice
+sudo apt update
+sudo apt upgrade
+# Thanks for using my script!
